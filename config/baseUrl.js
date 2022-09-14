c9f3ee7ccac031068fde779ffff7ea97fd620722
Said Ait Driss <saidaitdrissofficial@gmail.com>
2022-09-14 15:01:14 +0100

    Update baseUrl.js

--- config/baseUrl.js
+++ config/baseUrl.js
@@ -4,5 +4,5 @@ export const url = axios.create({
     baseURL:"https://api.pexels.com/v1",
 });
 
-export const apiKey = "563492ad6f917000010000015451d162b97542378cba816f8e462664";
+export const apiKey = "";
 
